9d9845cd2b24686579421074b1fc8f7a2380eab8
Akshit jaiswal <jaiswalakshit4@gmail.com>
2025-08-05 13:16:43 +0530

    Add files via upload

---
+++ fc.java
@@ -0,0 +1,28 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+
+public class SumExample {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
+
+
+public class IfElseExample {
+    public static void main(String[] args) {
+        int number = 7;
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
